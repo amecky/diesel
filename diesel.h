f91c52dfe5a02cf850e3ac4bd31d2b1d113d60a5
Andreas Mecky <a.mecky@verband.creditreform.de>
2017-02-01 15:14:03 +0100

    first step

---
+++ diesel.h
@@ -0,0 +1,467 @@
+#pragma once
+#include <stdint.h>
+
+namespace ds {
+
+	typedef struct {
+
+		float values[4];
+		struct {
+			float r;
+			float g;
+			float b;
+			float a;
+		};
+
+		operator float* () {
+			return &values[0];
+		}
+
+		operator const float* () const {
+			return &values[0];
+		}
+
+	} Color;
+
+	typedef struct {
+		uint16_t width;
+		uint16_t height;
+		Color clearColor;
+		const char* title;
+	} RenderSettings;
+
+	bool init(const RenderSettings& settings);
+
+	void begin();
+
+	void end();
+
+	bool isRunning();
+
+	void shutdown();
+
+}
+
+#ifdef DS_IMPLEMENTATION
+
+#include <Windows.h>
+#include <crtdbg.h>  
+#include <d3d11.h>
+//#include <d3dx11.h>
+//#include <DxErr.h>
+// This function was inspired by:
+
+namespace ds {
+
+	// http://www.rastertek.com/dx11tut03.html
+	static bool QueryRefreshRate(UINT screenWidth, UINT screenHeight, bool vsync, DXGI_RATIONAL* rational) {
+		DXGI_RATIONAL refreshRate = { 0, 1 };
+		if (vsync)
+		{
+			IDXGIFactory* factory;
+			IDXGIAdapter* adapter;
+			IDXGIOutput* adapterOutput;
+			DXGI_MODE_DESC* displayModeList;
+
+			// Create a DirectX graphics interface factory.
+			HRESULT hr = CreateDXGIFactory(__uuidof(IDXGIFactory), (void**)&factory);
+			if (FAILED(hr))
+			{
+				MessageBox(0,
+					TEXT("Could not create DXGIFactory instance."),
+					TEXT("Query Refresh Rate"),
+					MB_OK);
+
+				return false;
+			}
+
+			hr = factory->EnumAdapters(0, &adapter);
+			if (FAILED(hr))
+			{
+				MessageBox(0,
+					TEXT("Failed to enumerate adapters."),
+					TEXT("Query Refresh Rate"),
+					MB_OK);
+
+				return false;
+			}
+
+			hr = adapter->EnumOutputs(0, &adapterOutput);
+			if (FAILED(hr))
+			{
+				MessageBox(0,
+					TEXT("Failed to enumerate adapter outputs."),
+					TEXT("Query Refresh Rate"),
+					MB_OK);
+
+				return false;
+			}
+
+			UINT numDisplayModes;
+			hr = adapterOutput->GetDisplayModeList(DXGI_FORMAT_B8G8R8A8_UNORM, DXGI_ENUM_MODES_INTERLACED, &numDisplayModes, nullptr);
+			if (FAILED(hr))
+			{
+				MessageBox(0,
+					TEXT("Failed to query display mode list."),
+					TEXT("Query Refresh Rate"),
+					MB_OK);
+
+				return false;
+			}
+
+			displayModeList = new DXGI_MODE_DESC[numDisplayModes];
+			//assert(displayModeList);
+
+			hr = adapterOutput->GetDisplayModeList(DXGI_FORMAT_B8G8R8A8_UNORM, DXGI_ENUM_MODES_INTERLACED, &numDisplayModes, displayModeList);
+			if (FAILED(hr))
+			{
+				MessageBox(0,
+					TEXT("Failed to query display mode list."),
+					TEXT("Query Refresh Rate"),
+					MB_OK);
+
+				return false;
+			}
+
+			// Now store the refresh rate of the monitor that matches the width and height of the requested screen.
+			for (UINT i = 0; i < numDisplayModes; ++i)
+			{
+				if (displayModeList[i].Width == screenWidth && displayModeList[i].Height == screenHeight)
+				{
+					refreshRate = displayModeList[i].RefreshRate;
+				}
+			}
+
+			delete[] displayModeList;
+			if (adapterOutput) {
+				adapterOutput->Release();
+			}
+			if (adapter) {
+				adapter->Release();
+			}
+			if (factory) {
+				factory->Release();
+			}
+		}
+
+		*rational = refreshRate;
+		//LOG << "refresh: " << refreshRate.Numerator << " " << refreshRate.Denominator;
+		return true;
+	}
+
+
+
+	typedef struct {
+		HWND hwnd;
+		HINSTANCE instance;
+		uint16_t screenWidth;
+		uint16_t screenHeight;
+		Color clearColor;
+		bool running;
+		D3D_DRIVER_TYPE driverType;
+		D3D_FEATURE_LEVEL featureLevel;
+
+		ID3D11Device* d3dDevice;
+		ID3D11DeviceContext* d3dContext;
+		IDXGISwapChain* swapChain;
+		ID3D11RenderTargetView* backBufferTarget;
+
+		ID3D11Texture2D* depthTexture;
+		ID3D11DepthStencilView* depthStencilView;
+
+		ID3D11DepthStencilState* depthDisabledStencilState;
+		ID3D11DepthStencilState* depthEnabledStencilState;
+	} InternalContext;
+
+	static InternalContext* _ctx;
+
+	bool isRunning() {
+		MSG msg = { 0 };
+		if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		return _ctx->running;
+	}
+
+
+
+	static bool initializeDevice(const RenderSettings& settings) {
+		_ctx->clearColor = settings.clearColor;
+		//_context->depthEnabled = true;
+		RECT dimensions;
+		GetClientRect(_ctx->hwnd, &dimensions);
+
+		D3D_DRIVER_TYPE driverTypes[] = {
+			D3D_DRIVER_TYPE_HARDWARE, D3D_DRIVER_TYPE_WARP,
+			D3D_DRIVER_TYPE_REFERENCE, D3D_DRIVER_TYPE_SOFTWARE
+		};
+
+		unsigned int totalDriverTypes = ARRAYSIZE(driverTypes);
+
+		D3D_FEATURE_LEVEL featureLevels[] = {
+			D3D_FEATURE_LEVEL_11_0,
+			D3D_FEATURE_LEVEL_10_1,
+			D3D_FEATURE_LEVEL_10_0
+		};
+
+		unsigned int totalFeatureLevels = ARRAYSIZE(featureLevels);
+
+
+
+		DXGI_SWAP_CHAIN_DESC swapChainDesc;
+		ZeroMemory(&swapChainDesc, sizeof(swapChainDesc));
+		swapChainDesc.BufferCount = 1;
+		swapChainDesc.BufferDesc.Width = _ctx->screenWidth;
+		swapChainDesc.BufferDesc.Height = _ctx->screenHeight;
+		swapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+		DXGI_RATIONAL refreshRate;
+		QueryRefreshRate(_ctx->screenWidth, _ctx->screenHeight, true, &refreshRate);
+		//swapChainDesc.BufferDesc.RefreshRate.Numerator = 60;
+		//swapChainDesc.BufferDesc.RefreshRate.Denominator = 1;
+		swapChainDesc.BufferDesc.RefreshRate = refreshRate;
+		swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+		swapChainDesc.OutputWindow = _ctx->hwnd;
+		swapChainDesc.Windowed = true;
+		swapChainDesc.SampleDesc.Count = 4;
+		swapChainDesc.SampleDesc.Quality = 0;
+
+		unsigned int creationFlags = 0;
+
+#ifdef _DEBUG
+		creationFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif
+
+		HRESULT result;
+		unsigned int driver = 0;
+
+		for (driver = 0; driver < totalDriverTypes; ++driver) {
+			result = D3D11CreateDeviceAndSwapChain(0, driverTypes[driver], 0, creationFlags,
+				featureLevels, totalFeatureLevels,
+				D3D11_SDK_VERSION, &swapChainDesc, &_ctx->swapChain, &_ctx->d3dDevice, &_ctx->featureLevel, &_ctx->d3dContext);
+
+			if (SUCCEEDED(result)) {
+				_ctx->driverType = driverTypes[driver];
+				break;
+			}
+		}
+
+		if (FAILED(result)) {
+			//DXTRACE_MSG("Failed to create the Direct3D device!");
+			return false;
+		}
+
+		ID3D11Texture2D* backBufferTexture;
+
+		result = _ctx->swapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&backBufferTexture);
+
+		if (FAILED(result)) {
+			//DXTRACE_MSG("Failed to get the swap chain back buffer!");
+			return false;
+		}
+
+		result = _ctx->d3dDevice->CreateRenderTargetView(backBufferTexture, 0, &_ctx->backBufferTarget);
+
+		if (backBufferTexture)
+			backBufferTexture->Release();
+
+		if (FAILED(result))
+		{
+			//DXTRACE_MSG("Failed to create the render target view!");
+			return false;
+		}
+
+
+
+		D3D11_TEXTURE2D_DESC depthTexDesc;
+		ZeroMemory(&depthTexDesc, sizeof(depthTexDesc));
+		depthTexDesc.Width = _ctx->screenWidth;
+		depthTexDesc.Height = _ctx->screenHeight;
+		depthTexDesc.MipLevels = 1;
+		depthTexDesc.ArraySize = 1;
+		depthTexDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+		depthTexDesc.SampleDesc.Count = 4;
+		depthTexDesc.SampleDesc.Quality = 0;
+		depthTexDesc.Usage = D3D11_USAGE_DEFAULT;
+		depthTexDesc.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+		depthTexDesc.CPUAccessFlags = 0;
+		depthTexDesc.MiscFlags = 0;
+
+		result = _ctx->d3dDevice->CreateTexture2D(&depthTexDesc, 0, &_ctx->depthTexture);
+		if (FAILED(result)) {
+			//DXTRACE_MSG("Failed to create the depth texture!");
+			return false;
+		}
+
+		D3D11_DEPTH_STENCIL_VIEW_DESC descDSV;
+		ZeroMemory(&descDSV, sizeof(descDSV));
+		descDSV.Format = depthTexDesc.Format;
+		descDSV.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2DMS;
+		descDSV.Texture2D.MipSlice = 0;
+
+		result = _ctx->d3dDevice->CreateDepthStencilView(_ctx->depthTexture, &descDSV, &_ctx->depthStencilView);
+		if (FAILED(result)) {
+			//DXTRACE_MSG("Failed to create the depth stencil view!");
+			return false;
+		}
+
+		_ctx->d3dContext->OMSetRenderTargets(1, &_ctx->backBufferTarget, _ctx->depthStencilView);
+
+		D3D11_VIEWPORT viewport;
+		viewport.Width = static_cast<float>(_ctx->screenWidth);
+		viewport.Height = static_cast<float>(_ctx->screenHeight);
+		viewport.MinDepth = 0.0f;
+		viewport.MaxDepth = 1.0f;
+		viewport.TopLeftX = 0.0f;
+		viewport.TopLeftY = 0.0f;
+
+		_ctx->d3dContext->RSSetViewports(1, &viewport);
+
+		//_ctx->viewportCenter.x = _ctx->screenWidth / 2;
+		//_ctx->viewportCenter.y = _ctx->screenHeight / 2;
+
+		D3D11_DEPTH_STENCIL_DESC depthDisabledStencilDesc;
+		ZeroMemory(&depthDisabledStencilDesc, sizeof(depthDisabledStencilDesc));
+
+		// Now create a second depth stencil state which turns off the Z buffer for 2D rendering.  The only difference is 
+		// that DepthEnable is set to false, all other parameters are the same as the other depth stencil state.
+		depthDisabledStencilDesc.DepthEnable = false;
+		depthDisabledStencilDesc.DepthWriteMask = D3D11_DEPTH_WRITE_MASK_ALL;
+		depthDisabledStencilDesc.DepthFunc = D3D11_COMPARISON_LESS;
+		depthDisabledStencilDesc.StencilEnable = true;
+		depthDisabledStencilDesc.StencilReadMask = 0xFF;
+		depthDisabledStencilDesc.StencilWriteMask = 0xFF;
+		depthDisabledStencilDesc.FrontFace.StencilFailOp = D3D11_STENCIL_OP_KEEP;
+		depthDisabledStencilDesc.FrontFace.StencilDepthFailOp = D3D11_STENCIL_OP_INCR;
+		depthDisabledStencilDesc.FrontFace.StencilPassOp = D3D11_STENCIL_OP_KEEP;
+		depthDisabledStencilDesc.FrontFace.StencilFunc = D3D11_COMPARISON_ALWAYS;
+		depthDisabledStencilDesc.BackFace.StencilFailOp = D3D11_STENCIL_OP_KEEP;
+		depthDisabledStencilDesc.BackFace.StencilDepthFailOp = D3D11_STENCIL_OP_DECR;
+		depthDisabledStencilDesc.BackFace.StencilPassOp = D3D11_STENCIL_OP_KEEP;
+		depthDisabledStencilDesc.BackFace.StencilFunc = D3D11_COMPARISON_ALWAYS;
+
+		// Create the state using the device.
+		result = _ctx->d3dDevice->CreateDepthStencilState(&depthDisabledStencilDesc, &_ctx->depthDisabledStencilState);
+		if (FAILED(result)) {
+			return false;
+		}
+
+		depthDisabledStencilDesc.DepthEnable = true;
+		result = _ctx->d3dDevice->CreateDepthStencilState(&depthDisabledStencilDesc, &_ctx->depthEnabledStencilState);
+		if (FAILED(result)) {
+			return false;
+		}
+
+		//_context->viewMatrix = ds::matrix::m4identity();
+		//_context->projectionMatrix = ds::matrix::mat4OrthoLH(static_cast<float>(settings.screenWidth), static_cast<float>(settings.screenHeight), 0.1f, 100.0f);
+		//_context->viewProjectionMatrix = _context->viewMatrix * _context->projectionMatrix;
+
+		//_context->camera = 0;
+		//_context->orthoCamera = new ds::OrthoCamera(graphics::getScreenWidth(), graphics::getScreenHeight());
+		//_context->fpsCamera = new ds::FPSCamera(graphics::getScreenWidth(), graphics::getScreenHeight());
+
+		//ds::Viewport vw;
+		//vw.setDimension(graphics::getScreenWidth(), graphics::getScreenHeight());
+		//_context->viewports.push_back(vw);
+		//_context->selectedViewport = 0;
+		return true;
+	}
+
+	LRESULT CALLBACK WinProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
+		switch (message) {
+		case WM_CLOSE:
+			_ctx->running = false;
+			return 0;
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			return 0;
+		}
+		return DefWindowProc(hwnd, message, wParam, lParam);
+	}
+
+	bool init(const RenderSettings& settings) {
+
+		int flag = _CrtSetDbgFlag(_CRTDBG_REPORT_FLAG); // Get current flag
+		flag |= _CRTDBG_LEAK_CHECK_DF; // Turn on leak-checking bit
+		flag |= _CRTDBG_CHECK_ALWAYS_DF; // Turn on CrtCheckMemory
+										 //flag |= _CRTDBG_DELAY_FREE_MEM_DF;
+		_CrtSetDbgFlag(flag); // Set flag to the new value
+
+		_ctx = new InternalContext;
+		_ctx->instance = GetModuleHandle(NULL);
+		_ctx->screenWidth = settings.width;
+		_ctx->screenHeight = settings.height;
+
+		WNDCLASSEX wndClass = { 0 };
+		wndClass.cbSize = sizeof(WNDCLASSEX);
+		wndClass.style = CS_HREDRAW | CS_VREDRAW;
+		wndClass.lpfnWndProc = WinProc;
+		wndClass.hInstance = _ctx->instance;
+		wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+		wndClass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+		wndClass.lpszMenuName = NULL;
+		wndClass.lpszClassName = "D11";
+		if (!RegisterClassEx(&wndClass)) {
+			return false;
+		}
+		RECT DesktopSize;
+		GetClientRect(GetDesktopWindow(), &DesktopSize);
+		_ctx->hwnd = CreateWindow("D11", "D11",
+			WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+			(DesktopSize.right - settings.width) / 2, (DesktopSize.bottom - settings.height) / 2,
+			settings.width, settings.height,
+			NULL, NULL, _ctx->instance, NULL);
+
+		if (!_ctx->hwnd) {
+			DWORD dw = GetLastError();
+			//LOG << "Failed to created window";
+			//ErrorExit(TEXT("CreateWindow"));
+			//ExitProcess(dw);
+			return false;
+		}
+		RECT rect = { 0, 0, settings.width, settings.height };
+		AdjustWindowRect(&rect, GetWindowLong(_ctx->hwnd, GWL_STYLE), FALSE);
+		SetWindowPos(_ctx->hwnd, HWND_TOP, 0, 0, rect.right - rect.left, rect.bottom - rect.top,
+			SWP_NOZORDER | SWP_NOMOVE);
+		ShowWindow(_ctx->hwnd, SW_SHOW);
+		UpdateWindow(_ctx->hwnd);
+
+		SetWindowText(_ctx->hwnd, settings.title);
+
+		return initializeDevice(settings);
+	}
+
+	void shutdown() {
+		if (_ctx != 0) {
+			if (_ctx->backBufferTarget) _ctx->backBufferTarget->Release();
+			if (_ctx->swapChain) _ctx->swapChain->Release();
+			if (_ctx->d3dContext) _ctx->d3dContext->Release();
+			if (_ctx->depthStencilView) _ctx->depthStencilView->Release();
+			if (_ctx->depthTexture) _ctx->depthTexture->Release();
+			if (_ctx->depthDisabledStencilState) _ctx->depthDisabledStencilState->Release();
+			if (_ctx->depthEnabledStencilState) _ctx->depthEnabledStencilState->Release();
+			if (_ctx->d3dDevice) _ctx->d3dDevice->Release();
+			delete _ctx;
+		}
+	}
+
+	// ------------------------------------------------------
+	// begin rendering
+	// ------------------------------------------------------
+	void begin() {
+		_ctx->d3dContext->OMSetRenderTargets(1, &_ctx->backBufferTarget, _ctx->depthStencilView);
+		_ctx->d3dContext->ClearRenderTargetView(_ctx->backBufferTarget, _ctx->clearColor);
+		_ctx->d3dContext->ClearDepthStencilView(_ctx->depthStencilView, D3D11_CLEAR_DEPTH, 1.0f, 0.0);
+		//_context->selectedBlendState = 0;
+		//turnOnZBuffer();
+		//_context->sprites->begin();
+	}
+
+	// ------------------------------------------------------
+	// end rendering
+	// ------------------------------------------------------
+	void end() {
+		_ctx->swapChain->Present(0, 0);
+	}
+}
+#endif
