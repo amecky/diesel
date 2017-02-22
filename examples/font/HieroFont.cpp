ef189ee581c5f7f57bdd4bd7d9b2597b305a4bf9
meckya <amecky@gmail.com>
2017-02-22 12:28:11 +0100

    bitmap font example

---
+++ examples/font/HieroFont.cpp
@@ -0,0 +1,84 @@
+#include "HieroFont.h"
+#include <fstream>
+#include <vector>
+
+// char id=10    x=0    y=0    width=0    height=0    xoffset=0    yoffset=0    xadvance=0    page=0    chnl=0 
+int HieroFont::extractInt(const char* txt) {
+	// find = signs
+	const char* c = txt;
+	while (*c != '=') {
+		++c;
+	}
+	++c;
+	return atoi(c);
+}
+
+bool HieroFont::load(const char* fileName) {
+	std::ifstream file(fileName);
+	if (!file) {
+		printf("file not found!\n");
+		return false;
+	}
+	char cmd[256] = { 0 };
+	int cnt = 0;
+	int current = -1;
+	for (;; ) {
+		file >> cmd;
+		if (!file) {
+			break;
+		}
+		else if (strncmp(cmd, "id=",3) == 0) {
+			current = extractInt(cmd);
+			++cnt;
+		}
+		else if (strncmp(cmd, "width=", 6) == 0) {
+			if (current != -1) {
+				HieroFontItem& item = _items[current];
+				item.width = extractInt(cmd);
+			}
+		}
+		else if (strncmp(cmd, "height=", 6) == 0) {
+			if (current != -1) {
+				HieroFontItem& item = _items[current];
+				item.height = extractInt(cmd);
+			}
+		}
+		else if (strncmp(cmd, "x=", 2) == 0) {
+			if (current != -1) {
+				HieroFontItem& item = _items[current];
+				item.x = extractInt(cmd);
+			}
+		}
+		else if (strncmp(cmd, "y=", 2) == 0) {
+			if (current != -1) {
+				HieroFontItem& item = _items[current];
+				item.y = extractInt(cmd);
+			}
+		}
+		else if (strncmp(cmd, "xadvance=", 9) == 0) {
+			if (current != -1) {
+				HieroFontItem& item = _items[current];
+				item.xadvance = extractInt(cmd);
+			}
+		}
+		else if (strncmp(cmd, "yoffset=", 7) == 0) {
+			if (current != -1) {
+				HieroFontItem& item = _items[current];
+				item.yoffset = extractInt(cmd);
+			}
+		}
+		else if (strncmp(cmd, "xoffset=", 7) == 0) {
+			if (current != -1) {
+				HieroFontItem& item = _items[current];
+				item.xoffset = extractInt(cmd);
+			}
+		}
+	}
+	// nasty workaround for space (char 32). Since Hiero exports it with 0 dimension
+	HieroFontItem& item = _items[32];
+	item.x = 0;
+	item.y = 96;
+	item.width = 12;
+	item.height = 32;
+	return true;
+}
--- examples/font/main.cpp
+++ examples/font/main.cpp
@@ -1,8 +1,11 @@
 #define DS_IMPLEMENTATION
 #include "..\..\diesel.h"
-#define STB_TRUETYPE_IMPLEMENTATION  // force following include to generate implementation
-#include "stb_truetype.h"
-
+#include "HieroFont.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+/*
+	Bitmap font demo. The font is generated using Hiero: https://github.com/libgdx/libgdx/wiki/Hiero
+*/
 // ---------------------------------------------------------------
 // Rect
 // ---------------------------------------------------------------
@@ -58,23 +61,17 @@ struct SpriteConstantBuffer {
 	matrix wvp;
 };
 
-// ---------------------------------------------------------------
-// get angle between two v2 vectors
-// ---------------------------------------------------------------
-float getAngle(const v2& u, const v2& v) {
-	double x = v.x - u.x;
-	double y = v.y - u.y;
-	double ang = atan2(y, x);
-	return (float)ang;
-}
+const int MAX_SPRITES = 256;
+Sprite sprites[MAX_SPRITES];
+int numSprites;
 
 // ---------------------------------------------------------------
 // create sprite
 // ---------------------------------------------------------------
-int add(const v2& p, const Rect& r, Sprite* sprites, int index) {
-	if ((index + 1) < 64) {
-		float angle = 0.0f;// ds::random(0.0f, ds::PI * 2.0f);
-		Sprite& s = sprites[index];
+void add(const v2& p, const Rect& r) {
+	if ((numSprites + 1) < MAX_SPRITES) {
+		float angle = 0.0f;
+		Sprite& s = sprites[numSprites++];
 		s.position = p;
 		s.color = ds::Color(1.0f, 1.0f, 1.0f, 1.0f);
 		s.scale = v2(1.0f, 1.0f);
@@ -84,45 +81,36 @@ int add(const v2& p, const Rect& r, Sprite* sprites, int index) {
 		float vx = cos(angle) * vel;
 		float vy = sin(angle) * vel;
 		s.velocity = v2(vx,vy);
-		return index + 1;
 	}
-	return index;
 }
 
+// ---------------------------------------------------------------
+// add text
+// ---------------------------------------------------------------
+void addText(const HieroFont& font, const v2& pos, const char* text) {
+	float xpos = pos.x;
+	float ypos = pos.y;
+	const char* current = text;
+	for (int i = 0; i < strlen(text); ++i) {
+		const HieroFontItem& item = font.get(*current);
+		float dimX = item.width;
+		float dimY = item.height;
+		add(v2(xpos + dimX * 0.5f, ypos + dimY * 0.5f), Rect(item.y, item.x, item.width, item.height));
+		xpos += dimX + 4;
+		++current;
+	}
+}
 // ---------------------------------------------------------------
 // main method
 // ---------------------------------------------------------------
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline, int iCmdshow) {
 	
-	unsigned char* ttf_buffer = new unsigned char[1 << 20];
-	unsigned char temp_bitmap[512 * 512];
+	HieroFont font;
+	font.load("times_new_roman_32.fnt");
 
-	stbtt_bakedchar cdata[96]; // ASCII 32..126 is 95 glyphs
-	
-	fread(ttf_buffer, 1, 1 << 20, fopen("nulshock.ttf", "rb"));
-	stbtt_BakeFontBitmap(ttf_buffer, 0, 32.0, temp_bitmap, 512, 512, 32, 96, cdata); // no guarantee this fits!
-	delete[] ttf_buffer;
-
-
-	Sprite sprites[64];
-	int numSprites = 0;
-
-	const char* text = "Hello world";
-	// create some sprites
-	float x = 100.0f;
-	float y = 384.0f;
-	float xpos = 100.0f;
-	float ypos = 384.0f;
-	for (int i = 0; i < strlen(text); ++i) {
-		const char* current = text;
-		stbtt_aligned_quad q;
-		
-		stbtt_GetBakedQuad(cdata, 512, 512, *current - 32, &xpos, &ypos, &q, 0);//1=opengl & d3d10+,0=d3d9
-		numSprites = add(v2(xpos, ypos), Rect(q.y0,q.x0,q.x1 - q.x0,q.y1 - q.y0), sprites, numSprites);
-		x += xpos;
-		++current;
-		//++numSprites;
-	}
+	addText(font, v2(100.0f, 300.0f), "Hello world");
+	addText(font, v2(300.0f, 500.0f), "More text is here");
+	addText(font, v2(600.0f, 100.0f), "Here is also some text");
 
 	SpriteConstantBuffer constantBuffer;
 	constantBuffer.screenDimension = v4(1024.0f, 768.0f, 512.0f, 512.0f);
@@ -139,10 +127,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline,
 	ds::init(rs);
 
 	// load image using stb_image
-	//int x, y, n;
-	//unsigned char *data = stbi_load("cubes.png", &x, &y, &n, 4);
-	RID textureID = ds::createTexture(512, 512, 1, temp_bitmap, ds::TextureFormat::R8_UINT);
-	//stbi_image_free(data);
+	int x, y, n;
+	unsigned char *data = stbi_load("times_new_roman_32.png", &x, &y, &n, 4);
+	RID textureID = ds::createTexture(x, y, n, data, ds::TextureFormat::R8G8B8A8_UNORM);
+	stbi_image_free(data);
 
 
 	RID bs_id = ds::createBlendState(ds::BlendStates::SRC_ALPHA, ds::BlendStates::SRC_ALPHA, ds::BlendStates::INV_SRC_ALPHA, ds::BlendStates::INV_SRC_ALPHA, true);
@@ -164,7 +152,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline,
 	RID vertexDeclId = ds::createVertexDeclaration(decl, 4, shaderID);
 		
 	RID cbid = ds::createConstantBuffer(sizeof(SpriteConstantBuffer));
-	RID vertexBufferID = ds::createVertexBuffer(ds::BufferType::DYNAMIC, 256, vertexDeclId);
+	RID vertexBufferID = ds::createVertexBuffer(ds::BufferType::DYNAMIC, MAX_SPRITES, vertexDeclId);
 	RID ssid = ds::createSamplerState(ds::TextureAddressModes::CLAMP, ds::TextureFilters::LINEAR);
 
 	// create orthographic view
@@ -174,7 +162,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline,
 
 	// batch descriptor used to initialize batch
 	ds::BatchDescriptor desc;
-	desc.size = 32;
+	desc.size = MAX_SPRITES;
 	desc.vertexBufferID = vertexBufferID;
 	desc.primitiveType = ds::PrimitiveTypes::POINT_LIST;
 	desc.vertexDeclarationID = vertexDeclId;
@@ -182,23 +170,6 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline,
 	ds::Batch<SpriteVertex> batch(desc);
 
 	while (ds::isRunning()) {
-		// move sprites
-		/*
-		for (int i = 0; i < numSprites; ++i) {
-			Sprite& s = sprites[i];
-			s.position += s.velocity * static_cast<float>(ds::getElapsedSeconds());
-			if (s.position.x < 10.0f || s.position.x > 1000.0f) {
-				s.velocity.x *= -1.0f;
-				s.position += s.velocity * static_cast<float>(ds::getElapsedSeconds());
-				s.rotation = getAngle(s.velocity, v2(1, 0));
-			}
-			if (s.position.y < 10.0f || s.position.y > 760.0f) {
-				s.velocity.y *= -1.0f;
-				s.position += s.velocity * static_cast<float>(ds::getElapsedSeconds());
-				s.rotation = getAngle(s.velocity, v2(1, 0));
-			}
-		}
-		*/
 		ds::begin();
 		// disbale depth buffer
 		ds::setDepthBufferState(ds::DepthBufferState::DISABLED);
